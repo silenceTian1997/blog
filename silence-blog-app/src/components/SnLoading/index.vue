cccbc378ab2a0f51a5c090151baa27f1eec7049a
‘zhan‘‘tianshengnangit config --global user.email ‘i572031583@qq.com <572031583@qq.com>
2023-05-16 23:09:51 +0800

    动画测试

--- silence-blog-app/src/components/SnLoading/index.vue
+++ silence-blog-app/src/components/SnLoading/index.vue
@@ -1,90 +1,162 @@
 <script lang='ts' setup>
 import gsap from 'gsap'
-import { snakeCase } from 'lodash';
-import { onMounted } from 'vue'
+import { onMounted , ref } from 'vue'
 // console.log()
+const loadingFlag = ref(true)
 onMounted(() => {
+    // loadingFlag.value = true
+    const cirCleDomList: NodeListOf<HTMLDListElement> = document.querySelectorAll('.icon-circle')
+    
+    cirCleDomList.forEach((el: HTMLDListElement,index:number) => {
+        if(index === cirCleDomList.length - 1){
+            moveElementToCenter(el,()=>{
+                const timeLine = gsap.timeline({
+                    onComplete:()=>{
+                        loadingFlag.value = false
+                    }
+                })
+                cirCleDomList.forEach((el)=>{
+                    timeLine.to(el,{opacity:0 , delay:.5})
+                })
+                timeLine.to('.center-circle',{
+                    opacity:1,
+                    delay:1.8,
+                    rotate:360,
+                    duration:4,
+                    ease:'expoScale(1, 2)'
+                })
+                timeLine.to('.center-circle',{
+                    scale:4,
+                    duration:4,
+                    transition:2
+                })
+                timeLine.to('.center-text',{
+                    color:'#fea4b0',
+                    opacity:1,
+                    duration:2,
+                    fontSize:80,
+                })
+                timeLine.to('.center-text',{
+                    duration:1,
+                    opacity:0
+                })
+                timeLine.to('.center-circle',{
+                    width:'100%',
+                    height:"100%",
+                    background:'rebeccapurple',
+                    transformOrigin:'50% 50%',
+                    duration:1,
+                    transition:2,
+                })
+                timeLine.to('.center-circle',{
+                    opacity:0,
+                    background:'#f7f7f7',
+                    duration:1,
+                    transition:2
+                })
+            })
 
-  const cirCleDomList: NodeListOf<HTMLDListElement> = document.querySelectorAll('.icon-circle')
+        }else{
+            moveElementToCenter(el)
+        }
+    })
+    
+    // const timeLine = gsap.timeline()
+    // timeLine.add()
 
-  // gsap.to()
-
-
-  cirCleDomList.forEach((el: HTMLDListElement) => {
-
-    moveElementToCenter(el)
-
-  })
-
-  function moveElementToCenter(element: HTMLDListElement) {
-    let x, y, positionX, positionY;
-    const centerX = (window.innerWidth - element.offsetWidth) / 2;
-    const centerY = (window.innerHeight - element.offsetHeight) / 2;
-    if (element.style.left && element.style.top) {
-      x = parseInt(element.style.left, 10);
-      y = parseInt(element.style.top, 10);
-    } else {
-      let rect = element.getBoundingClientRect();
-      x = rect.left;
-      y = rect.top;
+    function moveElementToCenter(element: HTMLDListElement , cb?:Function) {
+        let x, y, positionX, positionY;
+        const centerX = (window.innerWidth - element.offsetWidth) / 2;
+        const centerY = (window.innerHeight - element.offsetHeight) / 2;
+        if (element.style.left && element.style.top) {
+            x = parseInt(element.style.left, 10);
+            y = parseInt(element.style.top, 10);
+        } else {
+            let rect = element.getBoundingClientRect();
+            x = rect.left;
+            y = rect.top;
+        }
+        positionX = centerX - x;
+        positionY = centerY - y
+        gsap.fromTo(element, { x: x, y: y, opacity: 0, scale: .2, ease: "power1", rotation: 0,  }, { x: positionX, y: positionY, opacity: 1, scale: 2, ease: "power1", rotation: 360, transition: 2,onComplete: cb && cb() })
     }
-    positionX = centerX - x;
-    positionY = centerY - y
-    gsap.fromTo(element, { x: x, y: y, opacity: 0, scale: .2, ease: "power1", delay: 1 }, { x: positionX, y: positionY, opacity: 1, scale: 2, ease: "power1", delay: 1, transition: 2, })
-  }
 
 })
 </script>
 
 <template>
-  <div class="sn-loading">
-    <div class="icon-circle"></div>
-    <div class="icon-circle"></div>
-    <div class="icon-circle"></div>
-    <div class="icon-circle"></div>
-  </div>
+    <div class="sn-loading" >
+        <div class="icon-circle"></div>
+        <div class="icon-circle"></div>
+        <div class="icon-circle"></div>
+        <div class="icon-circle"></div>
+
+        <div class="center-circle">
+
+            <span class="center-text">♥</span>
+        </div>
+
+
+    </div>
 </template>
 <style lang='scss' scoped>
 @include b(loading) {
-  position: fixed;
-  width: 100%;
-  height: 100%;
-  top: 0;
-  left: 0;
-  background-color: #fff;
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    background-color: #fff;
 
-  .icon-circle {
-    width: 40px;
-    height: 40px;
-    border-radius: 50%;
-    overflow: hidden;
-    position: absolute;
-    box-shadow: inset -5px -1px 8px 3px rgba(217, 168, 7, 0.85), 0px 2px 4px 0px rgba(0, 0, 0, 0.25), inset 10px 10px 8px 1px rgba(239, 240, 233, 0.62);
+    .icon-circle {
+        width: 40px;
+        height: 40px;
+        border-radius: 50%;
+        overflow: hidden;
+        position: absolute;
+        box-shadow: inset -5px -1px 8px 3px rgba(217, 168, 7, 0.85), 0px 2px 4px 0px rgba(0, 0, 0, 0.25), inset 10px 10px 8px 1px rgba(239, 240, 233, 0.62);
 
-    &:nth-child(1) {
-      top: 0;
-      left: 0;
-      background-color: #ffb84c;
+        &:nth-child(1) {
+            top: 0;
+            left: 0;
+            background-color: #ffb84c;
+        }
+
+        &:nth-child(2) {
+            top: 0;
+            right: 0;
+            background-color: #f266ab;
+        }
+
+        &:nth-child(3) {
+            bottom: 0;
+            left: 0;
+            background-color: #a459d1;
+        }
+
+        &:nth-child(4) {
+            bottom: 0;
+            right: 0;
+            background-color: #2cd3e1;
+
+        }
     }
-
-    &:nth-child(2) {
-      top: 0;
-      right: 0;
-      background-color: #f266ab;
+    
+    .center-circle{
+        position: absolute;
+        width: 200px;
+        height: 200px;
+        left: calc(50% - 100px);
+        top: calc(50% - 100px);
+        background-color: #2cd3e1;
+        border-radius: 20px;
+        opacity: 0;
+        text-align: center;
+        line-height: 200px;
+        .center-text{
+            opacity: 0;
+            letter-spacing: 4px;
+        }
     }
-
-    &:nth-child(3) {
-      bottom: 0;
-      left: 0;
-      background-color: #a459d1;
-    }
-
-    &:nth-child(4) {
-      bottom: 0;
-      right: 0;
-      background-color: #2cd3e1;
-
-    }
-  }
 }
 </style>
